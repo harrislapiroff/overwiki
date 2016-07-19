312bf6f9502a8924a24d7d385fd06ee4b075dbbc
Harris Lapiroff <harris@chromamine.com>
2016-07-19 11:37:41 -0400

    Use whitenoise to include static assets

--- overwiki/settings.py
+++ overwiki/settings.py
@@ -125,6 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
