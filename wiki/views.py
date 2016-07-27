66dc842fe4289134d24cdd97b1266b3345bcde66
Harris Lapiroff <harris@chromamine.com>
2016-07-26 22:49:36 -0400

    Add permissions to API

--- wiki/views.py
+++ wiki/views.py
@@ -2,6 +2,7 @@ from django.views.generic import DetailView, UpdateView, CreateView
 from django.shortcuts import redirect
 from django.http import Http404
 from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from reversion.views import RevisionMixin
 from wiki.models import Page
 from wiki.serializers import PageSerializer
@@ -10,6 +11,7 @@ from wiki.serializers import PageSerializer
 class PageViewSet(viewsets.ModelViewSet):
     queryset = Page.objects.all()
     serializer_class = PageSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
 
 class WikiContextMixin(object):
