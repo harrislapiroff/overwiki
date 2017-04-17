720a44fbc4e7ed0877b6d54adc6fcd3b2f3dcd98
Harris Lapiroff <harris@chromamine.com>
2017-04-17 19:37:37 -0400

    Add webpack pipeline
    * Clear out all non-api python views
    * Clear out all django templates
    * Clear out all previous frontend files
    * Introduce webpack config
    * Introduce appropraite package.json
    * Clear out requirements.txt and reintroduce appropriate files
    * Rewrite .gitignore
    * Update README
    * Clean up settings.py

---
+++ webpack.config.js
@@ -0,0 +1,134 @@
+var webpack       = require('webpack');
+var merge         = require('webpack-merge');
+var autoprefixer  = require('autoprefixer');
+var BundleTracker = require('webpack-bundle-tracker');
+var path = require('path');
+
+var TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+
+var target = __dirname + '/wiki/static/wiki/bundles';
+
+var postCSSPlugins = function() {
+	return {
+		defaults: [autoprefixer],
+		cleaner: [autoprefixer({ browsers: [
+			'Chrome >= 35',
+			'Firefox >= 31',
+			'Edge >= 12',
+			'Explorer >= 9',
+			'iOS >= 8',
+			'Safari >= 8',
+			'Android 2.3',
+			'Android >= 4',
+			'Opera >= 12'
+		]})]
+	}
+}
+
+var main = {
+	entry: {
+		main: __dirname + '/client/main.js',
+	},
+
+	output: {
+		path: target,
+		filename: '[name].js'
+	},
+
+	resolve: {
+		alias: {
+			'~': __dirname + '/client/js',
+			modernizr$: path.resolve(__dirname, '.modernizrrc')
+		},
+		extensions: ['.js', '.jsx'],
+		modules: ['node_modules']
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						query: {
+							presets: ['react', 'es2015', 'stage-0', 'stage-1', 'stage-2'],
+							plugins: ['add-module-exports']
+						},
+					}
+				],
+				include: [
+					path.join(__dirname, '/client'),
+				],
+			},
+			{
+				test: /\.s[ca]ss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: postCSSPlugins
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							includePaths: [path.resolve(__dirname, 'node_modules/')]
+						}
+					}
+				],
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: postCSSPlugins
+						}
+					}
+				]
+			},
+			// Currently unused, but we'll want it if we install modernizr:
+			{
+				test: /\.modernizrrc$/,
+				use: ['modernizr']
+			}
+		]
+	},
+
+	plugins: [
+		new BundleTracker({
+			path: target,
+			filename: './webpack-stats.json'
+		})
+	]
+};
+
+if (TARGET === 'build') {
+	module.exports = merge(main, {
+		output: {
+			filename: '[name]-[hash].js'
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env': { 'NODE_ENV': JSON.stringify('production') }
+			})
+		]
+	});
+}
+
+if (TARGET === 'start') {
+	module.exports = merge(main, {
+		devtool: 'eval-source-map',
+		devServer: {
+			contentBase: target,
+			progress: true,
+		}
+	});
+}
--- wiki/urls.py
+++ wiki/urls.py
@@ -9,11 +9,7 @@ router.register(r'pages', views.PageViewSet)
 
 
 urlpatterns = [
-    url(r'^react/.*', TemplateView.as_view(template_name="wiki/react_client.html")),
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include(router.urls, namespace='wiki')),
-    url(r'^new/$', views.PageCreateView.as_view(), name='page_create'),
-    url(r'^(?P<slug>[-\w]*)/$', views.PageView.as_view(), name='page_detail'),
-    url(r'^(?P<slug>[-\w]*)/edit/$', views.PageUpdateView.as_view(), name='page_update'),
-    url(r'^(?P<slug>)$', views.PageView.as_view()),
+    url(r'^.*', TemplateView.as_view(template_name="wiki/app.html")),
 ]
